452715427e0f87374d78bc6ed60977fbe4baf4fb
Mover12 <mover-12@yandex.ru>
2024-12-09 13:13:41 +0300

    add String functions

--- src/WiFiConnector.cpp
+++ src/WiFiConnector.cpp
@@ -2,31 +2,43 @@
 
 #include <cstring>
 
-void WiFiConnectorClass::setName(const char* APssid) {
-    memcpy(_APssid, APssid, strlen(APssid));
+void WiFiConnectorClass::setName(const char* ssid) {
+    memcpy(_ssid, ssid, strlen(ssid));
 }
 
-void WiFiConnectorClass::setPass(const char* APpass) {
-    memcpy(_APpass, APpass, strlen(APpass));
+void WiFiConnectorClass::setName(const String& ssid) {
+    setName(ssid.c_str());
 }
 
-void WiFiConnectorClass::onConnect(ConnectorCallback cb) {
+void WiFiConnectorClass::setPass(const char* pass) {
+    memcpy(_pass, pass, strlen(pass));
+}
+
+void WiFiConnectorClass::setPass(const String& pass) {
+    setPass(pass.c_str());
+}
+
+void WiFiConnectorClass::onConnect(WiFiConnectorCallback cb) {
     _conn_cb = cb;
 }
 
-void WiFiConnectorClass::onError(ConnectorCallback cb) {
+void WiFiConnectorClass::onError(WiFiConnectorCallback cb) {
     _err_cb = cb;
 }
 
 void WiFiConnectorClass::connect(const char* ssid, const char* pass) {
     WiFi.mode(WIFI_AP_STA);
-    WiFi.softAP(_APssid, _APpass);
+    WiFi.softAP(_ssid, _pass);
     WiFi.begin(ssid, pass);
 }
 
-void WiFiConnectorClass::begin(const char* APssid, const char* APpass) {
-    setName(APssid);
-    setPass(APpass);
+void WiFiConnectorClass::connect(const String& ssid, const String& pass) {
+    connect(ssid.c_str(), pass.c_str());
+}
+
+void WiFiConnectorClass::begin(const char* ssid, const char* pass) {
+    setName(ssid);
+    setPass(pass);
 
     WiFi.onEvent([](WiFiEvent_t event) {
         WiFi.enableAP(false);
@@ -37,6 +49,10 @@ void WiFiConnectorClass::begin(const char* APssid, const char* APpass) {
         WiFi.enableSTA(false);
         if (WiFiConnector._err_cb) WiFiConnector._err_cb();
     }, WIFI_EVENT_STAMODE_DISCONNECTED);
+}
+
+void WiFiConnectorClass::begin(const String& ssid, const String& pass) {
+    begin(ssid.c_str(), pass.c_str());
 }
 
 WiFiConnectorClass WiFiConnector;
--- src/WiFiConnector.h
+++ src/WiFiConnector.h
@@ -8,32 +8,36 @@
 #endif
 
 class WiFiConnectorClass {
-    typedef std::function<void()> ConnectorCallback;
+    typedef std::function<void()> WiFiConnectorCallback;
 
     public:
         // установить имя AP
-        void setName(const char* APssid);
+        void setName(const char* ssid);
+        void setName(const String& ssid);
 
         // установить пароль AP
-        void setPass(const char* APpass);
+        void setPass(const char* pass);
+        void setPass(const String& pass);
 
         // подключить обработчик успешного подключения
-        void onConnect(ConnectorCallback cb);
+        void onConnect(WiFiConnectorCallback cb);
 
         // подключить обработчик ошибки подключения, вызовется после старта AP
-        void onError(ConnectorCallback cb);
+        void onError(WiFiConnectorCallback cb);
+
+        // вызвать в setup один раз
+        void begin(const char* ssid="", const char* pass="");
+        void begin(const String& ssid, const String& pass);
 
         // подключиться. Вернёт false если ssid не задан, будет запущена AP
         void connect(const char* ssid, const char* pass="");
-
-        // вызвать в setup один раз
-        void begin(const char* APssid="", const char* APpass="");
+        void connect(const String& ssid, const String& pass="");
     private:
-        char _APssid[32];
-        char _APpass[64];
+        char _ssid[32];
+        char _pass[64];
 
-        ConnectorCallback _conn_cb = nullptr;
-        ConnectorCallback _err_cb = nullptr;
+        WiFiConnectorCallback _conn_cb = nullptr;
+        WiFiConnectorCallback _err_cb = nullptr;
 };
 
 extern WiFiConnectorClass WiFiConnector;
